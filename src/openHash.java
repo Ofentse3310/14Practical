545207e59263c629af024cf67336ae90d6624428
Cscience1 <angakolisile03@gmail.com>
2026-03-02 14:47:44 +0200

    Start hashing project Adding a delete function

--- src/openHash.java
+++ src/openHash.java
@@ -1,23 +1,49 @@
 import java.util.Objects;
 
 public class openHash {
-    static int size;
 
-    static int hash(int key){
+    static class openHashing {
 
-        return (key % size);
-    }
+        int[] arr;
+        static int size;
+
+        static int hash(int key) {
+
+            return (key % size);
+        }
 
 
-    static boolean search(int[] arr, int item){
-        int index = hash(item);
+        static boolean search(int[] arr, int item) {
+            int index = hash(item);
 
-        while(arr[index] != -1){
-            if(arr[index] == item){
-                return true;
+            while (arr[index] != -1) {
+                if (arr[index] == item) {
+                    return true;
+                }
+                index = (index + 1) % size;
+            }
+            return false;
+        }
+
+        public void insert(int item) {
+            int index = hash(item);
+
+            while (arr[index] != -1) {
+                index = (index + 1) % size;
+            }
+            arr[index] = item;
+        }
+
+        public static void delete(int[] arr,int item){
+            int index = hash(item);
+
+            while(arr[index] != -1){
+                if(arr[index] == item){
+                    arr[index] = -2;
+                    return;
+                }
             }
             index = (index + 1) % size;
         }
-        return false;
     }
 }
